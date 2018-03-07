209bd985760acabd9166dc4ec65ad30b0a695869
Carlos Saito <c.saito.style@gmail.com>
2018-03-07 16:24:09 +0100

    New page: resume

---
+++ pages/resume.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Timeline from './components/timeline';
+import Head from 'next/head';
+
+const Container = styled.main`
+  margin: auto;
+  padding: 16px;
+  max-width: 720px;
+`;
+
+export default () => (
+  <Container>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="stylesheet" href="/static/global.css" />
+    </Head>
+    <Timeline />
+  </Container>
+);

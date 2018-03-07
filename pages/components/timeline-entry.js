57963571b70784a8427ad8f4dbbb963cebdcbd49
Carlos Saito <c.saito.style@gmail.com>
2018-03-07 18:25:54 +0100

    Add "red line" in timeline entries

--- pages/components/timeline-entry.js
+++ pages/components/timeline-entry.js
@@ -2,6 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 import { format } from 'date-fns';
 
+const Container = styled.section`
+  position: relative;
+  border-top: #ccc 1px solid;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  margin-bottom: 16px;
+`;
+
+const UnstyledLine = ({ start, end, className }) => {
+  const zero = new Date('2009-03');
+  const now = new Date();
+
+  const total = now - zero;
+  const relativeStart = (start - zero) / total;
+  const relativeWidth = ((end || now) - start) / total;
+
+  return (
+    <div
+      className={className}
+      style={{
+        transform: `translateX(${relativeStart *
+          100}%) scaleX(${relativeWidth})`
+      }}
+    />
+  );
+};
+
+const Line = styled(UnstyledLine)`
+  transform-origin: left;
+  border-top: #f00 1px solid;
+  position: absolute;
+  top: -1px;
+  width: 100%;
+`;
+
 const Header = styled.header`
   color: #f00;
   font-weight: 700;
@@ -40,7 +75,8 @@ const Description = styled.div`
 const formatDate = date => date && format(date, 'YYYY/MM');
 
 export default ({ start, end, place, title, company, description }) => (
-  <div>
+  <Container>
+    <Line start={start} end={end} />
     <Header>
       <Time>
         {formatDate(start)} &gt; {end ? formatDate(end) : 'present'}
@@ -52,5 +88,5 @@ export default ({ start, end, place, title, company, description }) => (
       <Company>{company}</Company>
       <Description>{description}</Description>
     </div>
-  </div>
+  </Container>
 );
--- pages/components/timeline.js
+++ pages/components/timeline.js
@@ -4,19 +4,4 @@ import styled from 'styled-components';
 import data from './timeline.data';
 import Entry from './timeline-entry';
 
-const EntryContainer = styled.section`
-  border-top: #ccc 1px solid;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  margin-bottom: 16px;
-`;
-
-export default () => (
-  <div>
-    {data.map(entry => (
-      <EntryContainer>
-        <Entry {...entry} />
-      </EntryContainer>
-    ))}
-  </div>
-);
+export default () => <div>{data.map(entry => <Entry {...entry} />)}</div>;

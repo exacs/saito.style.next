38f3ed9d967315fd015006327a93f17be4192de2
Carlos Saito <c.saito.style@gmail.com>
2018-03-07 16:27:30 +0100

    Put Logo in resumé

--- pages/resume.js
+++ pages/resume.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Timeline from './components/timeline';
+import X from './components/x';
 import Head from 'next/head';
 
 const Container = styled.main`
@@ -10,12 +11,21 @@ const Container = styled.main`
   max-width: 720px;
 `;
 
+const Logo = styled.div`
+  width: 30px;
+`;
+
 export default () => (
   <Container>
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" href="/static/global.css" />
     </Head>
+    <Logo>
+      <a href="/">
+        <X />
+      </a>
+    </Logo>
     <Timeline />
   </Container>
 );
